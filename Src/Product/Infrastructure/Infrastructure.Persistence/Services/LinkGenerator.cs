6742b0af36032645ebcf5d59962622d643541e7d
mohammad yousefi <mohammadmy619@gmail.com>
2024-07-11 14:04:34 +0330

    fix product

---
+++ Src/Product/Infrastructure/Infrastructure.Persistence/Services/LinkGenerator.cs
@@ -0,0 +1,11 @@
+﻿using Application.ApplicationServices;
+
+namespace Infrastructure.Persistence.Services;
+public class LinkGenerator : ILinkGenerator
+{
+    public string Generate()
+    {
+        // TODO: implement a generator algorithm
+        return Guid.NewGuid().ToString();
+    }
+}
